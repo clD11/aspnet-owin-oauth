371f47aea6bdd1380d5c68636e30ba58f57b988b
kd17 <kit.davies@pinsentmasons.com>
2017-10-21 21:49:53 +0100

    Add project files.

---
+++ Owin.Security.HighQ/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Owin.Security.HighQ
+{
+    public class Class1
+    {
+    }
+}
